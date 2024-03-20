d4ee076386de9ecab7aa762112c3e58a022c5773
Edu Moreno <e.m.morenolp@gmail.com>
2024-03-20 17:16:17 -0300

    Arreglado la barra

--- scripts.js
+++ scripts.js
@@ -1,3 +1,23 @@
 
 console.log("Consola Asistentes Web")
 
+document.addEventListener("DOMContentLoaded", function () {
+    var links = document.querySelectorAll("nav a");
+
+    links.forEach(function (link) {
+        link.addEventListener("click", function (event) {
+
+            event.preventDefault();
+
+            var targetId = link.getAttribute("href");
+
+            var targetElement = document.querySelector(targetId);
+
+            var offset = targetElement.offsetTop - 140; 
+            window.scrollTo({
+                top: offset,
+                behavior: "smooth"
+            });
+        });
+    });
+});
